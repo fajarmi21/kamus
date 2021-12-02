a632d768cebf2a725e14f91dd08f39fa000d1820
fajar militan <fajarmilitan1@gmail.com>
2021-12-02 23:42:25 +0700

    update

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -12,17 +12,20 @@ class Home extends BaseController
     public function all()
     {
         $sql = $this->builder->table('kamus')
-        ->join('jenis', 'kamus.jenis = jenis.id_jenis')
-        ->get()->getResultArray();
-        echo json_encode($sql);
+        ->join('jenis', 'kamus.jenis = jenis.id_jenis');
+        if ($this->request->getPost('find') != null || $this->request->getPost('find') != "") {
+            $sql->like('kamus.b_inggris', $this->request->getPost('find'), 'both')
+                ->orLike('kamus.b_indo', $this->request->getPost('find'), 'both');
+        }
+        echo json_encode($sql->get()->getResultArray());
     }
 
     public function find()
     {
         $sql = $this->builder->table('kamus')
         ->join('jenis', 'kamus.jenis = jenis.id_jenis')
-        ->like('kamus.b_inggris', $this->request->getPost('find'))
-        ->orLike('kamus.b_indo', $this->request->getPost('find'))
+        ->like('kamus.b_inggris', $this->request->getPost('find'), 'both')
+        ->orLike('kamus.b_indo', $this->request->getPost('find'), 'both')
         ->get()->getResultArray();
         echo json_encode($sql);
     }
