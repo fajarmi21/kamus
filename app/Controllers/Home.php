9605ad84e4955b49a4d3e631a4a1bb8d1b9fdd3a
fajar militan <fajarmilitan1@gmail.com>
2021-12-02 19:46:40 +0700

    update

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,4 +8,22 @@ class Home extends BaseController
     {
         return view('welcome_message');
     }
+
+    public function all()
+    {
+        $sql = $this->builder->table('kamus')
+        ->join('jenis', 'kamus.jenis = jenis.id_jenis')
+        ->get()->getResultArray();
+        echo json_encode($sql);
+    }
+
+    public function find()
+    {
+        $sql = $this->builder->table('kamus')
+        ->join('jenis', 'kamus.jenis = jenis.id_jenis')
+        ->like('kamus.b_inggris', $this->request->getPost('find'))
+        ->orLike('kamus.b_indo', $this->request->getPost('find'))
+        ->get()->getResultArray();
+        echo json_encode($sql);
+    }
 }
