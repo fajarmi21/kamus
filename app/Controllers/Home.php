d59c197988c4260fdda0e280384e8a48df7551c6
fajar militan <fajarmilitan1@gmail.com>
2021-12-17 13:05:41 +0700

    update

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -14,17 +14,20 @@ class Home extends BaseController
         $array = array();
         $sql = $this->builder->table('kamus')
             ->join('jenis', 'kamus.jenis = jenis.id_jenis');
-        if ($this->request->getPost('find') != null || $this->request->getPost('find') != "") {
+        if ($this->request->getPost('find') !== null || $this->request->getPost('find') != "") {
             $array['jenis'] = $sql
-            ->select('jenis.b_ing, jenis.b_ind')
-            ->like('kamus.b_inggris', $this->request->getPost('find'), 'both')
-            ->orLike('kamus.b_indo', $this->request->getPost('find'), 'both')
-            ->get()->getResultArray();
+                ->select('jenis.b_ing, jenis.b_ind')
+                ->like('kamus.b_inggris', $this->request->getPost('find'), 'both')
+                ->orLike('kamus.b_indo', $this->request->getPost('find'), 'both')
+                ->get()->getResultArray();
             $array['kamus'] = $sql
-            ->select('kamus.b_inggris, kamus.b_indo')
-            ->like('kamus.b_inggris', $this->request->getPost('find'), 'both')
-            ->orLike('kamus.b_indo', $this->request->getPost('find'), 'both')
-            ->get()->getResultArray();
+                ->select('kamus.b_inggris, kamus.b_indo')
+                ->like('kamus.b_inggris', $this->request->getPost('find'), 'both')
+                ->orLike('kamus.b_indo', $this->request->getPost('find'), 'both')
+                ->get()->getResultArray();
+        } else {
+            $array = $sql->get()->getResultArray();
+
         }
         echo json_encode($array);
     }
